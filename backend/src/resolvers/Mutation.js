a39c38f1a0816411109a5aa90c7a143c6a1a1d14
haipayazoo <haipayazoo@gmail.com>
2020-05-27 16:25:16 -0700

    Creating the first query and mutation.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    // Create a dog!
+    const newDogs = { name: args.name };
+    global.dogs.push(newDogs);
+    return newDog;
+  },
+};
 
 module.exports = Mutations;
