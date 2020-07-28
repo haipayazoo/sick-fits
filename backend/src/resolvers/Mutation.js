88ccd209e0b061dd0158893c0b02df355d1ad822
Cameron Mathos <haipayazoo@gmail.com>
2020-07-28 13:59:12 -0700

    Implemented password reset in back end

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -118,6 +118,48 @@ const Mutations = {
         console.log(res);
         return { message: 'Thanks!' };
         // 3. Email them that reset token
+    },
+
+    async resetPassword(parent, args, ctx, info) {
+        // 1. Check if the passwords match
+        if (args.password !== args.confirmPassword) throw new Error('Passwords do not match!');
+
+        // 2. Check if it's a legitimate reset token
+
+        // 3. Check if it has expired
+        const [user] = await ctx.db.query.users({
+            where: {
+                resetToken: args.resetToken,
+                resetTokenExpiry_gte: Date.now() - 3600000
+            }
+        });
+
+        if (!user) throw new Error('This token is either invalid or expired!');
+
+        // 4. Hash their new password
+        const password = await bcrypt.hash(args.password, 10);
+
+        // 5. Save the new password to the user and remove old resetToken fields
+        const updatedUser = await ctx.db.mutation.updateUser({
+            where: { email: user.email },
+            data: {
+                password,
+                resetToken: null,
+                resetTokenExpiry: null
+            }
+        });
+
+        // 6. Generate JWT
+        const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+
+        // 7. Set the JWT cookie
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365
+        });
+
+        // 8. Return the new user
+        return updatedUser;
     }
 };
 
