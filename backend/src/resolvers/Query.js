757e091b280976b9a453c535ada59a6bff6af640
Cameron Mathos <haipayazoo@gmail.com>
2020-09-08 13:11:51 -0700

    Created a middleware the populates user per req

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,5 +1,5 @@
 const { forwardTo } = require('prisma-binding');
-const { hasPermissions } = require('../utils');
+const { hasPermission } = require('../utils');
 
 const Query = {
     items: forwardTo('db'),
@@ -21,10 +21,10 @@ const Query = {
             throw new Error('You must be logged in!');
         }
         // 2. Check if the user has the permissions to query all users
-        hasPermissions(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
 
         // 3. If they do, query the users
-        return ctx.db.users({}, info);
+        return ctx.db.query.users({}, info);
     }
 };
 
