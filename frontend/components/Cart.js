98c150de93231d22ed06dab231dc4b7dc5d05649
Cameron Mathos <haipayazoo@gmail.com>
2020-09-10 14:09:48 -0700

    Added local state and queries

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 import CartStyles from '../components/styles/CartStyles';
 import Supreme from '../components/styles/Supreme';
 import CloseButton from '../components/styles/CloseButton';
 import SickButton from '../components/styles/SickButton';
 
-const Cart = () => {
-    return (
-        <CartStyles open>
-            <header>
-                <CloseButton title="close">&times;</CloseButton>
-                <Supreme>Your Cart</Supreme>
-                <p>You have __ items in your cart.</p>
-            </header>
-            <footer>
-                <p>$10.00</p>
-                <SickButton>Checkout</SickButton>
-            </footer>
-        </CartStyles>
-    );
-};
+const LOCAL_STATE_QUERY = gql`
+    query {
+        cartOpen @client
+    }
+`;
+
+const TOGGLE_CART_MUTATION = gql`
+    mutation {
+        toggleCart @client
+    }
+`;
+
+const Cart = () => (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+        {toggleCart => (
+            <Query query={LOCAL_STATE_QUERY}>
+                {({ data }) => (
+                    <CartStyles open={data.cartOpen}>
+                        <header>
+                            <CloseButton onClick={toggleCart} title="close">
+                                &times;
+                            </CloseButton>
+                            <Supreme>Your Cart</Supreme>
+                            <p>You have __ items in your cart.</p>
+                        </header>
+                        <footer>
+                            <p>$10.00</p>
+                            <SickButton>Checkout</SickButton>
+                        </footer>
+                    </CartStyles>
+                )}
+            </Query>
+        )}
+    </Mutation>
+);
 
 export default Cart;
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
